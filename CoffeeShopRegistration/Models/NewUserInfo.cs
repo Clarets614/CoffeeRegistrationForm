89500480f01263044facf4faa27354659a528498
Clare S <Clare.Stock@gmail.com>
2024-05-30 08:42:05 -0400

    first commit

---
+++ CoffeeShopRegistration/Models/NewUserInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace CoffeeShopRegistration.Models
+{
+    public class NewUserInfo
+    {
+        public string firstName {  get; set; }
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+        public string password { get; set; }
+
+    }
+}
